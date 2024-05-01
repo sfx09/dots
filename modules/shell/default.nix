0bf811c11cb8a4eead9b0387188938544925b3b5
sfx09 <aleth@tuta.io>
2024-05-01 16:04:00 +0530

    system update

--- modules/shell/default.nix
+++ modules/shell/default.nix
@@ -53,16 +53,24 @@
         zrp = "zellij action rename-pane";
         zrt = "zellij action rename-tab";
         zrs = "zellij action rename-session";
+        zd = "zellij kill-all-sessions --yes; zellij delete-all-sessions --yes";
       };
       loginShellInit = ''test (tty) = "/dev/tty1"; and exec Hyprland'';
       interactiveShellInit = ''
         set -g fish_key_bindings fish_vi_key_bindings
         bind -M insert \cy accept-autosuggestion
+        bind -M insert \cf 'sessionizer; commandline -f repaint'
+        bind -M insert \cp up-or-search
+        bind -M insert \cn down-or-search
         set fish_greeting
       '';
     };
     starship.enable = true;
     zoxide.enable = true;
     man.generateCaches = false;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    }
   };
 }
