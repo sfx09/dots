445c472223782165ae4fc20c7a086d494b546773
sfx09 <aleth@tuta.io>
2024-07-27 17:55:08 +0530

    firefox: enable installation of extensions

--- modules/firefox/policies.nix
+++ modules/firefox/policies.nix
@@ -15,7 +15,6 @@
     Fingerprinting = true;
   };
   ExtensionSettings = {
-    "*".installation_mode = "blocked";
     "uBlock0@raymondhill.net" = {
       install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
       installation_mode = "force_installed";
